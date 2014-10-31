25011de1fdf668a6099918bb7e02ae9fbf9325ae
Roman Franchuk <franchukrom@gmail.com>
2014-10-31 21:34:25 +0200

    Remove any temporary testing logic from initdb.js

--- initdb.js
+++ initdb.js
@@ -1,26 +1,9 @@
 var dbaccess = require('./models/dbaccess')
 var user = require('./models/user')
 
-// FIXME: remove testing logic from here
 user.initTables(function(err) {
 	if (err) throw err;
 	console.log('Tables initialized');
-	user.save({
-		name: 'Roman',
-		surname: 'Franchuk'
-	}, function(err) {
-		if (err) throw err;
-		user.findById(1, function(err, row) {
-			if (err) throw err;
-			row['name'] = 'Serhiy';
-			user.save(row, function(err) {
-				if (err) throw err;
-				user.findById(1, function(err, row) {
-					console.log(row);
-					dbaccess.pool.end(function() {});
-				});
-			});
-		});
-	});
+	dbaccess.pool.end(function() {});
 });
 
