aa826f531891f3b5475ce3cab033cf0943d54d69
Roman Franchuk <franchukrom@gmail.com>
2014-11-01 00:32:34 +0200

    Fix CI: add support for empty password

--- models/dbaccess.js
+++ models/dbaccess.js
@@ -3,7 +3,7 @@ exports.pool  = mysql.createPool({
 	connectionLimit: 10,
 	host: process.env.DBP_HOST || 'localhost',
 	user: process.env.DBP_USER || 'db_project',
-	password: process.env.DBP_PASSWORD || 'db_password',
+	password: process.env.DBP_PASSWORD || process.env.DBP_USER ? '' : 'db_password',
 	database: process.env.DBP_DATABASE || 'db_project'
 });
 exports.prefix = 'dbp_';
