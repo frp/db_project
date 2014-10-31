a6793f4fd9c7af1ae6653d1ea78cb6a502d521e2
Roman Franchuk <franchukrom@gmail.com>
2014-10-31 18:05:41 +0200

    Add authorization and tests for user model

--- models/user.js
+++ models/user.js
@@ -75,17 +75,28 @@ exports.save = function(data, cb) {
 };
 
 exports.initTables = function(cb) {
-	pool.getConnection(function(err, conn) {
-		if (err) cb(err);
-		else {
-			conn.query('DROP TABLE ' + tableName, function(err, result) {
-				// Ignore error, it may mean that table exists
-				dbaccess.createTable(tableName, schema, function(err, result){
-					cb(err);
-				});
-			});
-		}
+	pool.query('DROP TABLE ' + tableName, function(err, result) {
+		// Ignore error, it may mean that table exists
+		dbaccess.createTable(tableName, schema, function(err, result){
+			cb(err);
+		});
 	});
 };
 
+exports.err_user_not_found = -1;
+exports.err_wrong_passwrod = -2;
+
+exports.authorization = function(login, password, cb) {
+	pool.query('SELECT user_id, password FROM ' + tableName + ' WHERE email = ?', [login], function(err, rows) {
+		if (err) throw err;
+		else {
+			if (typeof rows[0] == 'undefined')
+				cb(exports.err_user_not_found, -1);
+			else if (password != rows[0].password)
+				cb(exports.err_wrong_password, -1);
+			else
+				cb(null, rows[0].user_id)
+		}
+	});
+}
 
---
+++ tests/user.js
@@ -0,0 +1,68 @@
+var user = require('../models/user')
+var dbaccess = require('../models/dbaccess')
+
+function setUpDb(cb) {
+	return function(test) {
+		user.initTables(function(err) {
+			if (err) throw err;
+			cb(test);
+		});
+	}
+}
+
+function setUpUser(cb) {
+	return function(test) {
+		user.save({
+			name: 'User',
+			surname: 'Test',
+			email: 'test@gmail.com',
+			password: 'testpass'
+		}, function (err) {
+			if (err) throw err;
+			cb(test);
+		});
+	}
+}
+
+exports.testUpdating = setUpDb(setUpUser(function(test)	{
+	user.findById(1, function(err, row) {
+		if (err) throw err;
+		row['name'] = 'Serhiy';
+		user.save(row, function(err) {
+			if (err) throw err;
+			user.findById(1, function(err, row) {
+				if (err) throw err;
+				test.ok(row.name == 'Serhiy', 'test updating');
+				test.done();
+			});
+		});
+	});
+}));
+
+exports.testAuthorizationOK = setUpDb(setUpUser(function(test){
+	user.authorization('test@gmail.com', 'testpass', function(err, id) {
+		test.equals(id, 1);
+		test.equals(!err, true);
+		test.done();
+	});
+}));
+
+exports.testAuthorizationWrongUser = setUpDb(setUpUser(function(test) {
+	user.authorization('test2@gmail.com', 'testpass', function(err, id) {
+		test.equals(err, user.err_user_not_found);
+		test.done();
+	});
+}));
+
+exports.testAuthorizationWrongPassword = setUpDb(setUpUser(function(test) {
+	user.authorization('test@gmail.com', 'testvfgfdpass', function(err, id) {
+		test.equals(err, user.err_wrong_password);
+		test.done();
+	});
+}));
+
+exports.testKillDb = function(test) {
+	dbaccess.pool.end(function() {
+		test.done();
+	});
+}
