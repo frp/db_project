6c1bd7d36d3d6d43dc57a321f2ac21a6e6596de6
Roman Franchuk <franchukrom@gmail.com>
2014-11-20 16:28:47 +0200

    Switch from Nodeunit to Mocha.js

--- tests/flashmob.js
+++ tests/flashmob.js
@@ -2,60 +2,78 @@ var flashmob = require('../models/flashmob');
 var dbaccess = require('../models/dbaccess');
 var helpers = require('../testhelpers/dbhelpers.js');
 
-exports.testUpdating = helpers.setUpDb(helpers.setUpFlashmob(function(test)	{
-    flashmob.findById(1, function(err, row) {
-        if (err) throw err;
-        row['title'] = 'Changed title';
-        flashmob.save(row, function(err) {
-            if (err) throw err;
-            flashmob.findById(1, function(err, row) {
-                if (err) throw err;
-                test.ok(row.title == 'Changed title', 'test updating');
-                test.done();
-            });
+describe('Flashmob', function() {
+    beforeEach(helpers.setUpDb(function(done) {
+        done();
+    }));
+
+    it('should generate error if flashmob with such id does not exist', function(done) {
+        flashmob.findById(5, function(err, data) {
+            (data === null).should.be.true;
+            err.should.be.equal(dbaccess.err_record_not_found);
+            done();
         });
     });
-}));
 
-exports.testFlashmobNotFound = helpers.setUpDb(function(test) {
-    flashmob.findById(5, function(err, data) {
-        test.equals(data, null);
-        test.equals(err, dbaccess.err_record_not_found);
-        test.done();
-    });
-});
+    describe('Operations on existing flashmobs', function() {
+        beforeEach(helpers.setUpFlashmob(function(done) {
+            done();
+        }));
 
-exports.testAddingAMember = helpers.setUpDb(helpers.setUpFlashmob(helpers.setUpUser(function(test) {
-    flashmob.findById(1, function(err, data) {
-        data.addMember(1, 'admin', function(err) {
-            if (err) throw err;
-            flashmob.findById(1, function(err, data) {
+        it('should support updating', function(done) {
+            flashmob.findById(1, function(err, row) {
                 if (err) throw err;
-                data.getMembers(function(err, data) {
+                row['title'] = 'Changed title';
+                flashmob.save(row, function(err) {
                     if (err) throw err;
-                    test.equals(data.length, 1);
-                    test.done();
+                    flashmob.findById(1, function(err, row) {
+                        if (err) throw err;
+                        row.title.should.be.equal('Changed title')
+                        done();
+                    });
                 });
             });
         });
-    });
-})));
 
-exports.testDeletingAMember = helpers.setUpDb(helpers.setUpFlashmob(helpers.setUpUser(function(test) {
-    flashmob.findById(1, function(err, data) {
-        data.addMember(1, 'admin', function(err) {
-            if (err) throw err;
-            flashmob.findById(1, function(err, data) {
-                if (err) throw err;
-                data.deleteMember(1, 'admin', function(err) {
-                    if (err) throw err;
-                    data.getMembers(function(err, data) {
+        describe('Operations related to users', function() {
+            beforeEach(helpers.setUpUser(function(done) {
+                done();
+            }));
+
+            it ('should support adding members', function(done) {
+                flashmob.findById(1, function(err, data) {
+                    data.addMember(1, 'admin', function(err) {
                         if (err) throw err;
-                        test.equals(data.length, 0);
-                        test.done();
+                        flashmob.findById(1, function(err, data) {
+                            if (err) throw err;
+                            data.getMembers(function(err, data) {
+                                if (err) throw err;
+                                data.length.should.be.equal(1);
+                                done();
+                            });
+                        });
+                    });
+                });
+            });
+
+            it ('should support deleting members', function(done) {
+                flashmob.findById(1, function(err, data) {
+                    data.addMember(1, 'admin', function(err) {
+                        if (err) throw err;
+                        flashmob.findById(1, function(err, data) {
+                            if (err) throw err;
+                            data.deleteMember(1, 'admin', function(err) {
+                                if (err) throw err;
+                                data.getMembers(function(err, data) {
+                                    if (err) throw err;
+                                    data.length.should.be.equal(0);
+                                    done();
+                                });
+                            });
+                        });
                     });
                 });
             });
         });
     });
-})));
+});
--- tests/user.js
+++ tests/user.js
@@ -1,55 +1,68 @@
 var user = require('../models/user');
 var dbaccess = require('../models/dbaccess');
 var helpers = require('../testhelpers/dbhelpers.js');
+var should = require('should');
 
-exports.testUpdating = helpers.setUpDb(helpers.setUpUser(function(test)	{
-	user.findById(1, function(err, row) {
-		if (err) throw err;
-		row['name'] = 'Serhiy';
-		user.save(row, function(err) {
-			if (err) throw err;
+describe('User', function(done) {
+	beforeEach(helpers.setUpDb(function(done) {
+		done();
+	}));
+
+	it('should generate error when user does not exist', function(done) {
+		user.findById(5, function(err, data) {
+			(data === null).should.be.true;
+			err.should.be.equal(dbaccess.err_record_not_found);
+			done();
+		});
+	});
+
+	it('should check for required fields', function(done) {
+		user.save({email: 'aaa@bbb.com'}, function(err) {
+			err.should.be.equal(dbaccess.err_validation_failed);
+			done();
+		})
+	});
+
+	describe('Operations with existing user', function() {
+		beforeEach(helpers.setUpUser(function(done) {
+			done();
+		}));
+
+		it('should support updating', function(done) {
 			user.findById(1, function(err, row) {
 				if (err) throw err;
-				test.ok(row.name == 'Serhiy', 'test updating');
-				test.done();
+				row['name'] = 'Serhiy';
+				user.save(row, function(err) {
+					if (err) throw err;
+					user.findById(1, function(err, row) {
+						if (err) throw err;
+						row.name.should.be.equal('Serhiy');
+						done();
+					});
+				});
+			});
+		});
+
+		it('should support authorization', function(done) {
+			user.authorization('test@gmail.com', 'testpass', function(err, id) {
+				id.should.be.equal(1);
+				should(err).not.be.ok;
+				done();
+			});
+		});
+
+		it('should reject wrong user', function(done) {
+			user.authorization('test2@gmail.com', 'testpass', function(err, id) {
+				err.should.be.equal(dbaccess.err_record_not_found);
+				done();
+			});
+		});
+
+		it('should reject wrong password', function(done) {
+			user.authorization('test@gmail.com', 'testvfgfdpass', function(err, id) {
+				err.should.be.equal(user.err_wrong_password);
+				done();
 			});
 		});
 	});
-}));
-
-exports.testAuthorizationOK = helpers.setUpDb(helpers.setUpUser(function(test){
-	user.authorization('test@gmail.com', 'testpass', function(err, id) {
-		test.equals(id, 1);
-		test.equals(!err, true);
-		test.done();
-	});
-}));
-
-exports.testAuthorizationWrongUser = helpers.setUpDb(helpers.setUpUser(function(test) {
-	user.authorization('test2@gmail.com', 'testpass', function(err, id) {
-		test.equals(err, dbaccess.err_record_not_found);
-		test.done();
-	});
-}));
-
-exports.testAuthorizationWrongPassword = helpers.setUpDb(helpers.setUpUser(function(test) {
-	user.authorization('test@gmail.com', 'testvfgfdpass', function(err, id) {
-		test.equals(err, user.err_wrong_password);
-		test.done();
-	});
-}));
-
-exports.testUserNotFound = helpers.setUpDb(function(test) {
-	user.findById(5, function(err, data) {
-		test.equals(data, null);
-		test.equals(err, dbaccess.err_record_not_found);
-		test.done();
-	});
-});
-
-exports.testRequiredFields = helpers.setUpDb(function(test) {
-	user.save({email: 'aaa@bbb.com'}, function(err) {
-		test.equals(err, dbaccess.err_validation_failed);
-		test.done();
-	})
 });
