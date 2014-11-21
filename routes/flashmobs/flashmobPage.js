490db89e29691ecbbcbebb527d180dd5c8c028a1
Wolkdj <Wolkdj@gmail.com>
2014-11-21 14:41:50 +0200

    небольшие изменения для Дениса

--- routes/flashmobs/flashmobPage.js
+++ routes/flashmobs/flashmobPage.js
@@ -2,14 +2,15 @@ var Flashmob = require("../../models/flashmob")
 
 exports.get = function(req, res, next){
     var flashmobId = req.params.id
-    Flashmob.findById(flashmobId, function(err, flashmob){
+    res.render("flashmobPage")
+    /*Flashmob.findById(flashmobId, function(err, flashmob){
         if(err) res.send("not found")
         else{
             //var admin = (flashmob.organizer==req.session.id)
 
             res.render("flashmobPage")
         }
-    })
+    })*/
 }
 exports.post = function(req, res, next){
 // TODO: are we need this method?
